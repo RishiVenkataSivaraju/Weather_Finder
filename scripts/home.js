314c959a1129c2c7e4e3b9c243eda3891e02c9f6
Rishi Sivaraju <sivarajurishi@gmail.com>
2024-02-19 10:29:58 +0530

    Avoiding OverFetching

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,5 @@
 const div = document.querySelector("#map")
+const info = document.querySelector(".info")
 mapboxgl.accessToken = 'pk.eyJ1IjoicmlzaGktc2l2YXJhanUiLCJhIjoiY2xrZ3o0MG5jMDRqdzNvcnp4ZnBvZHQ4bCJ9.fjkl7FXHgXdueyxgY2tnUA';
 // const divs = new mapboxgl.Map({
 //     container: 'map', // container ID
@@ -18,6 +19,10 @@ map.on('click', async (e) => {
     let lngLat = e.lngLat;
     let lat = lngLat.lat;
     let lon = lngLat.lng;
+    let con = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/ ${lon},${lat}.json?access_token=pk.eyJ1IjoicmlzaGktc2l2YXJhanUiLCJhIjoiY2xrZ3o0MG5jMDRqdzNvcnp4ZnBvZHQ4bCJ9.fjkl7FXHgXdueyxgY2tnUA`)
+    console.log(con)
     let res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=bb5de4977205403a59132f425bfb51a2&units=metric`);
-    console.log(res.data.main.temp)
+    let Temperature = res.data.main.temp
+    info.innerHTML = `<b style="font-weight:bold; margin-left:20%; padding-top:30%;"> Temperature:</b>${Temperature}`
+    console.log(res.data)
 });
