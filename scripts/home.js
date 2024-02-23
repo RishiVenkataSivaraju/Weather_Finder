e339d9b7b08b81c5354fa31bb481e4ab0175f239
Rishi Sivaraju <sivarajurishi@gmail.com>
2024-02-23 19:23:08 +0530

    Css Added

--- scripts/home.js
+++ scripts/home.js
@@ -20,9 +20,32 @@ map.on('click', async (e) => {
     let lat = lngLat.lat;
     let lon = lngLat.lng;
     let con = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/ ${lon},${lat}.json?access_token=pk.eyJ1IjoicmlzaGktc2l2YXJhanUiLCJhIjoiY2xrZ3o0MG5jMDRqdzNvcnp4ZnBvZHQ4bCJ9.fjkl7FXHgXdueyxgY2tnUA`)
-    console.log(con)
+    let locality = con.data.features[1].place_name
+    console.log(locality)
     let res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=bb5de4977205403a59132f425bfb51a2&units=metric`);
+    let weather = res.data.weather[0].description
+    console.log(weather)
     let Temperature = res.data.main.temp
-    info.innerHTML = `<b style="font-weight:bold; margin-left:20%; padding-top:30%;"> Temperature:</b>${Temperature}`
-    console.log(res.data)
+    console.log(Temperature)
+    const view = document.createElement("p")
+    const temp = document.createElement("p")
+    temp.innerHTML = `Temperature:${Temperature}`
+    const weathers = document.createElement("p")
+    weathers.innerHTML = `<p style="margin-bottom:2%;">Weather:${weather}</p>`
+    const loc = document.createElement("p")
+    loc.innerHTML = `<p style="margin-bottom:20px;">Location:${locality}</p><hr>`
+    view.append(temp, weathers, loc)
+    info.append(view)
+    temp.classList.add("margin-inbetween");
+    info.classList.add("over")
+    view.classList.add("margin");
+});
+map.on('style.load', () => {
+  map.setFog({
+    color: 'rgb(186, 210, 235)', // Lower atmosphere
+    'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+    'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+    'space-color': 'rgb(11, 11, 25)', // Background color
+    'star-intensity': 0.6 // Background star brightness (default 0.35 at low zoooms )
+  });
 });
